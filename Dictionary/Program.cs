4ab124fee90bb44fbe17b96a969889be4f1c5d12
rupesh palve <rupesh.palve48@outlook.com>
2022-09-28 14:39:39 -0400

    sortList in generic

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -10,14 +10,36 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Dictionary<int,string> dictionary = new Dictionary<int,string>();
-            dictionary.Add(1, "dipali");
-            dictionary.Add(2,"rupali");
-            dictionary.Add(3, "shipali");
-            foreach(KeyValuePair<int,string> item in dictionary)
+            SortedList<string, int> sl = new SortedList<string, int>();
+
+            sl.Add("Rupesh",90);
+            sl.Add("Ajinkya",91);
+            sl.Add("Vijay", 93);
+            sl.Add("Sanjay", 95);
+            sl.Add("Tushar", 96);
+            sl.Add("Nilesh", 92);
+
+            sl.Remove("Vijay");
+
+           
+            Console.WriteLine(sl.Count);
+
+            foreach (var s in sl)
             {
-                Console.WriteLine(item.Key+" "+item.Value);
+                Console.WriteLine(s);
             }
         }
+    
+
+
+    //Dictionary<int,string> dictionary = new Dictionary<int,string>();
+    //dictionary.Add(1, "dipali");
+    //dictionary.Add(2,"rupali");
+    //dictionary.Add(3, "shipali");
+    //foreach(KeyValuePair<int,string> item in dictionary)
+    //{
+    //    Console.WriteLine(item.Key+" "+item.Value);
+    //}
+
     }
 }
