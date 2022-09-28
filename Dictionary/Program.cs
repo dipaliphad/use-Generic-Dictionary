9e308a066d4b0298e2642125ce69ace8ef448140
rupesh palve <rupesh.palve48@outlook.com>
2022-09-28 13:41:14 -0400

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> dictionary = new Dictionary<int,string>();
+            dictionary.Add(1, "dipali");
+            dictionary.Add(2,"rupali");
+            dictionary.Add(3, "shipali");
+            foreach(KeyValuePair<int,string> item in dictionary)
+            {
+                Console.WriteLine(item.Key+" "+item.Value);
+            }
+        }
+    }
+}
